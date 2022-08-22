31bcea345084cc2d4e81944c2049da82656ed558
Sergey <jaxi@atma.company>
2022-08-22 16:38:06 +0500

    Изменил форму ввода

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './reset.css'
 import './index.css';
+import './sass/bitrixStyle.scss'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
